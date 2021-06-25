815a45966ae5cd2593304e3b28bbdac8f1b0235b
Tarn W. Burton <twburton@gmail.com>
2021-06-25 08:14:26 -0400

    Update examples and fix some typos

--- lisp/register-widgets.lisp
+++ lisp/register-widgets.lisp
@@ -2,17 +2,21 @@
 
 
 (jupyter-widgets:register-widgets
+  annotation
   backbone
   ball-and-stick
   base
+  buffer-representation
   cartoon
   dihedral
   licorice
   line
   ribbon
+  shape
   spacefill
   stage
   structure
   surface
-  trajectory)
+  trajectory
+  unitcell)
 
